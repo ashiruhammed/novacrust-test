1397b274b78d2154d94d2dfd22e9b688dff667b9
ashiruhammed <hammedashiru89@gmail.com>
2025-12-16 11:13:09 +0100

    design crypto cash widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CryptoSwap from '@/components/widgets/crypto-swap';
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='shadow py-10 px-16 rounded-xl bg-white'>
+      <main className='shadow py-10 px-16 rounded-xl bg-white w-full max-w-2xl'>
         <CryptoSwap />
       </main>
     </div>
